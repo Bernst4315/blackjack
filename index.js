1ca36c32229d5aec0dd7618a48b163eda43b12d6
Bernst4315 <bernst4315@gmail.com>
2025-12-18 12:29:32 -0700

    draw card function draws 2 cards at the start, then one afterwards

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const checkDeckBtn = document.getElementById('check-deck-btn')
 const drawCardBtn = document.getElementById("draw-card-btn")
 
 let deckId = ""
+let handSetUp = false
 
 /*
 new game button:
@@ -27,24 +28,38 @@ function newGame(){
     console.log("click")
     getDeck()
     //console.log(deckId)
+    //drawCard()
     
 }
 
 function getDeck(){
     fetch("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
-    .then(res => res.json())
-    .then(data => {
-        console.log(data)
-        deckId = data.deck_id
-        console.log(deckId)
-    })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            deckId = data.deck_id
+            console.log(deckId)
+        })
 }
 
 function drawCard(){
     console.log("you drew a card")
-    fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`)
-        .then(res => res.json())
-        .then(data => console.log(data))
+    if(!handSetUp){
+
+        handSetUp = true
+        
+        fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`)
+            .then(res => res.json())
+            .then(data => console.log(data))
+
+            
+
+    } else {
+
+        fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`)
+            .then(res => res.json())
+            .then(data => console.log(data))
+    }
 }
 
 function checkDeck(){
