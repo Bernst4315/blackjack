71fde5f642b6d55abd704d76b1e5aacae7aec3aa
Bernst4315 <bernst4315@gmail.com>
2025-12-17 19:39:14 -0700

    new game button grabs a deck from the deck api

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-console.log("hello")
+const newGameBtn = document.getElementById("new-game-btn"); 
+
+const deckId = ""
+
+/*
+new game button:
+
+get a deck from api
+set game board
+keep track if game started = true 
+    if clicked when true, user prompt tht doing so will restart game
+    suffle deck for new game 
+
+
+*/
+
+newGameBtn.addEventListener("click", newGame)
+
+
+function newGame(){
+    console.log("click")
+    getDeck()
+    
+}
+
+function getDeck(){
+    fetch("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+        })
+}
